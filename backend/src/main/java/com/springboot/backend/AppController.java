d6975eb7e407fd0d32c2d9a7771ffa5797e28b95
gitam4 <321810306031@gitam.in>
2022-02-15 00:44:21 +0530

    page-done

--- backend/src/main/java/com/springboot/backend/AppController.java
+++ backend/src/main/java/com/springboot/backend/AppController.java
@@ -1,42 +1,35 @@
 package com.springboot.backend;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 
-    // @Autowired
-    // private UserRepository repo;
-    
+    @Autowired
+    private UserRepository repo;
+
     @GetMapping("")
-    public String viewHomePage()
-    {
+    public String viewHomePage() {
         return "index";
     }
 
-//     @GetMapping("/register")
-//     public String showRegistrationForm(Model model) 
-//     {
-//      model.addAttribute("user", new User());
-//      return "signup_form";
-//     }
+    @GetMapping("/register")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
 
-//     @PostMapping("/process_register")
-//     public String processRegister(User user) 
-//     {
-//     // BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//     // String encodedPassword = passwordEncoder.encode(user.getPassword());
-//     // user.setPassword(encodedPassword);
-     
-//     repo.save(user);
-     
-//     return "register_success";
-// }
+        return "signup_form";
+    }
 
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
 
-    
+        repo.save(user);
+
+        return "register_success";
+    }
+
 }
